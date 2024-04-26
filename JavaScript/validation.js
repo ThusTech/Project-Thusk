0a6e5fa37d5d0394b9dff894878363f97e71d9ef
thulani malaza <tmalaza429@gmail.com>
2024-04-26 16:10:45 +0200

    Added validation.js and modified other files

---
+++ JavaScript/validation.js
@@ -0,0 +1,21 @@
+const checkButton = document.querySelector(".submit");
+
+checkButton.onclick = demo;
+
+function demo(){
+    let answer = document.getElementById("answer");
+    console.log(answer);
+}
+
+// TODO:
+// function multipleOfThreeOrFive(){
+
+// }
+
+// function evenFibonacciNumbers(){
+
+// }
+
+// function largestPrimeFactor(){
+
+// }
