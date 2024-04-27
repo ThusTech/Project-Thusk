77747b12dee080b3b27f8390c7ec3697b002fe29
Thulani Malaza <thmalaz022@student.wethinkcode.co.za>
2024-04-27 19:44:26 +0200

    added validation js file

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,7 @@
-const question1 = document.querySelector(".question-1");
-const question2 = document.querySelector(".question-2");
-const question3 = document.querySelector(".question-3");
+const question1 = document.querySelector("#question-1");
+const question2 = document.querySelector("#question-2");
+const question3 = document.querySelector("#question-3");
+const input = document.querySelector("#answer-form");
 
 
 const description = document.querySelector(".description");
--- JavaScript/validation.js
+++ JavaScript/validation.js
@@ -1,21 +1,35 @@
 const checkButton = document.querySelector(".submit");
+const inputValue = document.querySelector("#answer");
+const inputForm = document.querySelector("#answer-form");
+const display = document.querySelector("#answer-response");
+const question = document.querySelector(".question");
 
-checkButton.onclick = demo;
+checkButton.onclick = action;
 
-function demo(){
-    let answer = document.getElementById("answer");
-    console.log(answer);
+function action(){
+    let value = inputValue.value;
+
+    //hide input div
+    inputForm.classList.replace("show-form","hide-form");
+    display.classList.replace("hide-response","show-response");
+
+    if(question.getAttribute("id") === "question-1"){
+        multipleOfThreeOrFive(value);
+    }else if(question.getAttribute("id") === "question-2"){
+        evenFibonacciNumbers(value);
+    }else if(question.getAttribute("id") === "question-3"){
+        largestPrimeFactor(value);
+    }
 }
 
-// TODO:
-// function multipleOfThreeOrFive(){
+function multipleOfThreeOrFive(data){
+    display.innerText = "1";
+}
 
-// }
+function evenFibonacciNumbers(data){
+    display.innerText = "2";
+}
 
-// function evenFibonacciNumbers(){
-
-// }
-
-// function largestPrimeFactor(){
-
-// }
+function largestPrimeFactor(data){
+    display.innerText = "3";
+}
